4d540907f131a5e9f4bdb6331b603f4d21e2668d
Sam <sam.dao@YRB-DES007>
2019-07-23 16:47:07 +0800

    Update hello world

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 80;
-app.get('/', (req, res) => res.send({ hello: 'world 2222' }));
+app.get('/', (req, res) => res.send({ hello: 'world 333333' }));
 app.listen(port, () => console.log(`Listening on port: ${port}`));
